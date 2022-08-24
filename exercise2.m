ae44067fd1b1e5043058410096692c2d5c7637e3
Gene Raynor Z. Penino <103450270+raynorshineee@users.noreply.github.com>
2022-08-24 18:42:01 +0800

    Add files via upload

---
+++ exercise2.m
@@ -0,0 +1,189 @@
+# Created by Octave 7.1.0, Wed Aug 24 18:36:42 2022 GMT <unknown@DESKTOP-AJGDBRA>
+# name: ans
+# type: scalar struct
+# ndims: 2
+ 1 1
+# length: 32
+# name: Filename
+# type: sq_string
+# elements: 1
+# length: 25
+C:\Users\USER\fruits2.png
+
+
+# name: FileModDate
+# type: sq_string
+# elements: 1
+# length: 20
+24-Aug-2022 18:36:10
+
+
+# name: FileSize
+# type: scalar
+68
+
+
+# name: Format
+# type: sq_string
+# elements: 1
+# length: 3
+PNG
+
+
+# name: FormatVersion
+# type: sq_string
+# elements: 0
+
+
+# name: Width
+# type: scalar
+1
+
+
+# name: Height
+# type: scalar
+1
+
+
+# name: BitDepth
+# type: scalar
+16
+
+
+# name: ColorType
+# type: sq_string
+# elements: 1
+# length: 9
+grayscale
+
+
+# name: DelayTime
+# type: scalar
+0
+
+
+# name: DisposalMethod
+# type: sq_string
+# elements: 0
+
+
+# name: LoopCount
+# type: scalar
+0
+
+
+# name: ByteOrder
+# type: sq_string
+# elements: 1
+# length: 9
+undefined
+
+
+# name: Gamma
+# type: scalar
+0
+
+
+# name: Chromaticities
+# type: matrix
+# rows: 1
+# columns: 0
+
+
+
+# name: Comment
+# type: sq_string
+# elements: 0
+
+
+# name: Quality
+# type: scalar
+75
+
+
+# name: Compression
+# type: sq_string
+# elements: 1
+# length: 9
+undefined
+
+
+# name: Colormap
+# type: matrix
+# rows: 0
+# columns: 0
+
+
+# name: Orientation
+# type: scalar
+1
+
+
+# name: ResolutionUnit
+# type: sq_string
+# elements: 1
+# length: 9
+undefined
+
+
+# name: XResolution
+# type: scalar
+0
+
+
+# name: YResolution
+# type: scalar
+0
+
+
+# name: Software
+# type: sq_string
+# elements: 0
+
+
+# name: Make
+# type: sq_string
+# elements: 0
+
+
+# name: Model
+# type: sq_string
+# elements: 0
+
+
+# name: DateTime
+# type: sq_string
+# elements: 0
+
+
+# name: ImageDescription
+# type: sq_string
+# elements: 0
+
+
+# name: Artist
+# type: sq_string
+# elements: 0
+
+
+# name: Copyright
+# type: sq_string
+# elements: 0
+
+
+# name: DigitalCamera
+# type: scalar struct
+# ndims: 2
+ 1 1
+# length: 0
+
+
+# name: GPSInfo
+# type: scalar struct
+# ndims: 2
+ 1 1
+# length: 0
+
+
+
+
